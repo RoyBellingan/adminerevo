3be5256ae3684c56365f35d73d33682871648486
Roy Bellingan <roy@tech.techadsmedia.com>
2024-06-11 02:31:37 +0100

    a bit of an hack, but else is not really possible or easy to access adminer machiner to perform a simple sql...

--- adminer/download.inc.php
+++ adminer/download.inc.php
@@ -1,10 +1,30 @@
 <?php
-$TABLE = $_GET["download"];
-$fields = fields($TABLE);
-header("Content-Type: application/octet-stream");
-header("Content-Disposition: attachment; filename=" . friendly_url("$TABLE-" . implode("_", $_GET["where"])) . "." . friendly_url($_GET["field"]));
-$select = array(idf_escape($_GET["field"]));
-$result = $driver->select($TABLE, $select, array(where($_GET, $fields)), $select);
-$row = ($result ? $result->fetch_row() : array());
-echo $driver->value($row[0], $fields[$_GET["field"]]);
+
+if($_GET["format"] == "json"){
+    header("Content-Type: application/json");
+    if(isset($_GET["sql"])){
+        $result = $connection->query($_GET["sql"]);
+        echo json_encode($result->fetch_all(MYSQLI_ASSOC));
+        exit;
+    }else{
+        $TABLE = $_GET["download"];
+        $fields = fields($TABLE);
+        $select = array(idf_escape($_GET["field"]));
+        $result = $driver->select($TABLE, $select, array(where($_GET, $fields)), $select);
+        $row = ($result ? $result->fetch_row() : array());
+        json_encode($row);
+        exit;
+    }
+}else{
+    $TABLE = $_GET["download"];
+    $fields = fields($TABLE);
+    header("Content-Type: application/octet-stream");
+    header("Content-Disposition: attachment; filename=" . friendly_url("$TABLE-" . implode("_", $_GET["where"])) . "." . friendly_url($_GET["field"]));
+    $select = array(idf_escape($_GET["field"]));
+    $result = $driver->select($TABLE, $select, array(where($_GET, $fields)), $select);
+    $row = ($result ? $result->fetch_row() : array());
+    echo $driver->value($row[0], $fields[$_GET["field"]]);
+}
+
+
 exit; // don't output footer
